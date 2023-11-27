15af6444a6bea69e8dc87d0a0c2a9d789c023340
Surendra Raju <surendrarajukondraju@gmail.com>
2023-11-27 19:01:30 +0530

    advancevariable

--- 4-advancevariale.sh
+++ 4-advancevariale.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-name1=ramesh
-name2=suresh
-
-echo "hi,$name2"
-echo "hello,$name1"
-echo "how are you,$name2"
-echo "i am fine $name1, what about you?"
-echo "i am good $name2"
+echo "hi"
+echo "hello"
+echo "how are you"
+echo "i am fine, what about you?"
+echo "i am good"
 echo "what are you doing?"
